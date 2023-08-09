1c298f95151b86818c0499128dd1a79da59629c6
Mo9932 <mbakr9932@gmail.com>
2023-08-10 02:33:18 +0300

    complete clinic management system

---
+++ Clinic_management_sys/ADMIN.h
@@ -0,0 +1,518 @@
+#include "STD_TYPES.h"
+
+typedef struct rev
+{
+	u8 id;
+	struct rev* next;
+}rev;
+
+typedef struct patient
+{
+	u32 id;
+	u8 age;
+	s8 gender;
+	struct rev* slot;
+	struct patient *next;
+	s8 name[];
+}patient;
+
+
+void add_p(patient* head)
+{
+	patient* ptr=head;
+	patient* tmp1=head;
+	patient* tmp=malloc(sizeof(patient));
+	tmp->next=NULL;
+	
+	printf("enter the name:");
+	scanf("%s",&tmp->name);
+	
+	printf("enter the age:");
+	scanf("%u",&tmp->age);
+	printf("enter the gender M or F:");
+	scanf(" %c",&tmp->gender);
+	printf("enter the ID:");
+	scanf("%u",&tmp->id);
+	
+	while(ptr->next!=NULL)
+	{
+		ptr=ptr->next;
+	}
+	ptr->next=tmp;
+	
+	while(tmp1->next!=NULL)
+	{
+		while(tmp->id==tmp1->id)
+		{
+			printf("this ID is alrady existed\n enter another one:");
+			scanf("%u",&tmp->id);
+		}
+		tmp1=tmp1->next;
+	}
+	
+	
+}
+
+void edit_p(patient*p,u32 tmp)
+{
+	patient*ptr=p;
+	while(ptr!=NULL)
+	{
+		if(tmp==ptr->id)
+		{
+			printf("name: %s\n",ptr->name);
+			printf("age: %u\n",ptr->age);
+			printf("gender: %c\n",ptr->gender);
+			printf("edit the name:");
+			scanf("%s",&ptr->name);
+			printf("edit the age:");
+			scanf("%u",&ptr->age);
+			printf("edit the gender M or F:");
+			scanf(" %c",&ptr->gender);
+			break;
+		}
+		ptr=ptr->next;
+	}
+}
+
+void print_list(patient* head)
+{
+	u8 count=0;
+	
+	patient* ptr=head;
+	ptr=ptr->next;
+	printf("-------------------------------------\n");
+	while(ptr!=NULL)
+	{
+		printf("\tname: %s \n",ptr->name);
+		printf("\tage: %u \n",ptr->age);
+		printf("\tgender: %c \n",ptr->gender);
+		printf("\tID: %u \n",ptr->id);
+		printf("-------------------------------------\n");
+		ptr=ptr->next;
+		count++;
+	}
+	if(count==0)
+	{
+		printf("this list is empty\n");
+		printf("-------------------------------------\n");
+	}
+}
+
+void add_slot(rev* p,u8 x)
+{
+	rev* ptr=p;
+	rev* tmp=malloc(sizeof(rev));
+	tmp->id=x;
+	tmp->next=NULL;
+	while(ptr->next!=NULL)
+	{
+		ptr=ptr->next;
+	}
+	ptr->next=tmp;
+}
+void print_slot(rev* head)
+{
+	u8 count=0;
+	
+	rev* ptr=head;
+	rev* ptr1=head;
+	rev* ptr2=head;
+	rev* ptr3=head;
+	rev* ptr4=head;
+	printf("-------------------------------------\n");
+	while(ptr!=NULL)
+	{
+		if(count==0)
+		{
+			ptr=ptr->next;
+			count++;
+		}
+		else
+		{
+			switch((ptr->id)+1)
+			{
+				case 1:
+					printf("\tslot(%u):from 2pm to 2:30pm\n",(ptr->id)+1);
+					printf("-------------------------------------\n");
+					break;
+			}
+			count++;
+			ptr=ptr->next;
+		}
+	}
+	count=0;
+	while(ptr1!=NULL)
+	{
+		if(count==0)
+		{
+			ptr1=ptr1->next;
+			count++;
+		}
+		else
+		{
+			switch((ptr1->id)+1)
+			{
+				case 2:
+					printf("\tslot(%u):from 2:30pm to 3pm\n",(ptr1->id)+1);
+					printf("-------------------------------------\n");
+					break;
+			}
+			count++;
+			ptr1=ptr1->next;
+		}
+	}
+	count=0;
+	while(ptr2!=NULL)
+	{
+		if(count==0)
+		{
+			ptr2=ptr2->next;
+			count++;
+		}
+		else
+		{
+			switch((ptr2->id)+1)
+			{
+				case 3:
+					printf("\tslot(%u):from 3:30pm to 4pm\n",(ptr2->id)+1);
+					printf("-------------------------------------\n");
+					break;
+			}
+			count++;
+			ptr2=ptr2->next;
+		}
+	}
+	count=0;
+	while(ptr3!=NULL)
+	{
+		if(count==0)
+		{
+			ptr3=ptr3->next;
+			count++;
+		}
+		else
+		{
+			switch((ptr3->id)+1)
+			{
+				case 4:
+					printf("\tslot(%u):from 4pm to 4:30pm\n",(ptr3->id)+1);
+					printf("-------------------------------------\n");
+					break;
+			}
+			count++;
+			ptr3=ptr3->next;
+		}
+	}
+	count=0;
+	while(ptr4!=NULL)
+	{
+		if(count==0)
+		{
+			ptr4=ptr4->next;
+			count++;
+		}
+		else
+		{
+			switch((ptr4->id)+1)
+			{
+				case 5:
+					printf("\tslot(%u):from 4:30pm to 5pm\n",(ptr4->id)+1);
+					printf("-------------------------------------\n");
+					break;
+			}
+			count++;
+			ptr4=ptr4->next;
+		}
+	}
+	if(count==1)
+	{
+		printf("this list is empty\n");
+		printf("-------------------------------------\n");
+	}
+	
+}
+void print_p(patient* head,u8 tmp)
+{
+	u8 count=0;
+	patient*ptr=head;
+	while(ptr!=NULL)
+	{
+		if(tmp==ptr->id)
+		{
+			count++;
+			printf("name: %s\n",ptr->name);
+			printf("age: %u\n",ptr->age);
+			printf("gender: %c\n",ptr->gender);
+			break;
+		}
+		ptr=ptr->next;
+	}
+	if(count==0)
+		printf("\tno such ID\n");
+	printf("-------------------------------------\n");
+
+}
+void print_p_res(patient* head,u32 tmp)
+{
+	patient*ptr=head;
+	while(ptr!=NULL)
+	{
+		if(tmp==ptr->id)
+		{
+			printf("name: %s\n",ptr->name);
+			printf("age: %u\n",ptr->age);
+			printf("gender: %c\n",ptr->gender);
+			switch(ptr->slot->id)
+			{
+				case 0:
+					printf("\tfrom 2pm to 2:30pm\n");
+					printf("-------------------------------------\n");
+					break;
+				case 1:
+					printf("\tfrom 2:30pm to 3pm\n");
+					printf("-------------------------------------\n");
+					break;
+				case 2:
+					printf("\tfrom 3pm to 3:30pm\n");
+					printf("-------------------------------------\n");
+					
+					break;
+				case 3:
+					printf("\tfrom 4pm to 4:30p\n");
+					printf("-------------------------------------\n");
+					break;
+				case 4:
+					printf("\tfrom 4:30pm to 5pm\n");
+					printf("-------------------------------------\n");
+					break;
+				default:
+					printf("\tno reservation\n");
+					break;
+			}
+			break;
+		}
+		ptr=ptr->next;
+	}
+}
+void delet_slot(rev* slot,rev* un_slot,u8 id1)
+{
+	rev* ptr=slot;
+	rev* tmp=slot;
+	rev* tmp1=slot;
+	
+	while(ptr->next!=NULL)
+	{
+		tmp=tmp1->next;
+		
+		if(tmp->id==id1)
+		{
+			ptr->next=tmp->next;
+			add_slot(un_slot,tmp->id);
+			free(tmp);
+			tmp->next=NULL;
+			break;
+		}
+		else if(tmp->id!=id1)
+			ptr=ptr->next;
+		
+		tmp1=ptr;
+	}
+}
+
+void add_res(rev*x,u8 y,patient*p,u8 z,rev* head)
+{
+	u8 count=0;
+	
+	rev*ptr1=x;
+	patient*ptr=p;
+	
+	while(ptr!=NULL)
+	{
+		if(z==ptr->id)
+		{
+			count++;
+			break;
+		}
+		ptr=ptr->next;
+	}
+	
+	while(ptr1!=NULL)
+	{
+		if((y-1)==ptr1->id)
+		{
+			break;
+		}
+		ptr1=ptr1->next;
+	}
+	if(count==1)
+	{
+		ptr->slot=ptr1;
+		delet_slot(x,head,y-1);
+	}
+	else
+		printf("\tinvalid reservation\n");
+	printf("-------------------------------------\n");
+
+}
+
+void print_unslot(rev* head,patient* pt)
+{
+	u8 count=0;
+	patient* p=pt;
+	patient* p1=pt;
+	patient* p2=pt;
+	patient* p3=pt;
+	patient* p4=pt;
+	
+	rev* ptr=head;
+	rev* ptr1=head;
+	rev* ptr2=head;
+	rev* ptr3=head;
+	rev* ptr4=head;
+	
+	ptr=ptr->next;
+	p=p->next;
+	
+	ptr1=ptr1->next;
+	p1=p1->next;
+	
+	ptr2=ptr2->next;
+	p2=p2->next;
+	
+	ptr3=ptr3->next;
+	p3=p3->next;
+	
+	ptr4=ptr4->next;
+	p4=p4->next;
+	
+	printf("-------------------------------------\n");
+	while(ptr!=NULL)
+	{
+		
+		switch((ptr->id)+1)
+		{
+			case 1:
+				printf("\tslot(%u):from 2pm to 2:30pm\t",(ptr->id)+1);
+				while(p!=NULL)
+				{
+					if(p->slot->id==ptr->id)
+					{
+						count++;
+						printf("ID: %u\n",p->id);
+						printf("-------------------------------------\n");
+						break;
+					}
+					p=p->next;
+				}
+				break;
+		}
+		ptr=ptr->next;
+	}
+	
+	while(ptr1!=NULL)
+	{
+		
+		switch((ptr1->id)+1)
+		{
+			case 2:
+				printf("\tslot(%u):from 2:30pm to 3pm\t",(ptr1->id)+1);
+				while(p1!=NULL)
+				{
+					if(p1->slot->id==ptr1->id)
+					{
+						count++;
+						printf("ID: %u\n",p1->id);
+						printf("-------------------------------------\n");
+						break;
+					}
+					p1=p1->next;
+				}
+				break;
+		}
+		ptr1=ptr1->next;
+	}
+	
+	while(ptr2!=NULL)
+	{
+		
+	
+		switch((ptr2->id)+1)
+		{
+			case 3:
+				printf("\tslot(%u):from 3:30pm to 4pm\t",(ptr2->id)+1);
+				while(p2!=NULL)
+				{
+					if(p2->slot->id==ptr2->id)
+					{
+						count++;
+						printf("ID: %u\n",p2->id);
+						printf("-------------------------------------\n");
+						break;
+					}
+					p2=p2->next;
+				}
+				break;
+		}
+		ptr2=ptr2->next;
+	
+	}
+
+	while(ptr3!=NULL)
+	{
+		
+
+		switch((ptr3->id)+1)
+		{
+			case 4:
+				printf("\tslot(%u):from 4pm to 4:30pm\t",(ptr3->id)+1);
+				while(p3!=NULL)
+				{
+					if(p3->slot->id==ptr3->id)
+					{
+						count++;
+						printf("ID: %u\n",p3->id);
+						printf("-------------------------------------\n");
+						break;
+					}
+					p3=p3->next;
+				}
+				break;
+		}
+		ptr3=ptr3->next;
+	
+	}
+	
+	while(ptr4!=NULL)
+	{
+		
+		switch((ptr4->id)+1)
+		{
+			case 5:
+				printf("\tslot(%u):from 4:30pm to 5pm\t",(ptr4->id)+1);
+				while(p4!=NULL)
+				{
+					if(p4->slot->id==ptr4->id)
+					{
+						count++;
+						printf("ID: %u\n",p4->id);
+						printf("-------------------------------------\n");
+						break;
+					}
+					p4=p4->next;
+				}
+				break;
+		}
+		count++;
+		ptr4=ptr4->next;
+	
+	}
+	if(count==0)
+	{
+		printf("this list is empty\n");
+		printf("-------------------------------------\n");
+	}
+	
+}
+
+
---
+++ Clinic_management_sys/main.c
@@ -0,0 +1,189 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "STD_TYPES.h"
+#include "ADMIN.h"
+
+
+void main()
+{
+	u32 pass;
+	u8 count=1,state,tmp,tmp1,count1=0;
+	
+	patient *queue=malloc(sizeof(patient));
+	queue->next=NULL;
+	
+	rev *slot=malloc(sizeof(rev));
+	slot->next=NULL;
+	
+	rev *un_slot=malloc(sizeof(rev));
+	un_slot->next=NULL;
+	
+	for(int i=0;i<5;i++)
+	{
+		add_slot(slot,i);
+	}
+	
+	while(1)
+	{
+		printf("chose the mode:\n");
+		printf("enter 1 fore Admin mode\nenter 2 fore User mode\nenter 3 to exit\n");
+		
+		scanf("%u",&tmp);
+		
+		
+		//ADMIN mode starts here
+		
+		if(tmp==1)
+		{
+			printf("enter the password: ");
+			scanf("%u",&pass);
+			while(pass!=1234)
+			{
+				if(count==3)
+				{
+					printf("out of tries\n");
+					break;
+				}
+				else 
+				{
+					printf("wrong password\n try again: ");
+					scanf("%u",&pass);
+					count++;
+				}
+			}
+			if(pass==1234)
+				while(1)
+				{
+					if(count1==0)
+					{
+						printf("---------------------Welcome-------------------\n");
+						count1++;
+					}
+					
+					printf("\tto add pationt enter 1\n");
+					printf("\tto edit pationt enter 2\n");
+					printf("\tto add and edit reservation enter 3\n");
+					printf("\tto cancel reservation enter 4\n");
+					printf("\tto print queue enter 5\n");
+					printf("\tto print reservation enter 6\n");
+					printf("\tto print unavailable reservation enter 7\n");
+					printf("\tto print patient records enter 8\n");
+					printf("\tto exit enter 9\n");
+					printf("\tyour choise: ");
+					scanf("%u",&state);
+					printf("-----------------------------------------------\n");
+
+					switch(state)
+					{
+						case 1:
+							add_p(queue);
+							break;
+						case 2:
+							printf("enter the patient ID:");
+							scanf("%u",&tmp);
+							edit_p(queue,tmp);
+							break;
+						case 3:
+							printf("enter the patient ID:");
+							scanf("%u",&tmp1);
+							print_p(queue,tmp1);
+							printf("the available reservations are:\n ");
+							print_slot(slot);
+							printf("chose your reservation number:");
+							scanf("%u",&tmp);
+							add_res(slot,tmp,queue,tmp1,un_slot);	
+							break;
+						case 4:
+							printf("the unavailable reservations are:\n");
+							print_slot(un_slot);
+							printf("enter the number of reservation you want to free:");
+							scanf("%u",&tmp);
+							delet_slot(un_slot,slot,tmp-1);
+							break;
+						case 5:
+							printf("patient list:\n");
+							print_list(queue);
+							break;
+						case 6:
+							printf("the available reservations are:\n");
+							print_slot(slot);
+							break;
+						case 7:
+							printf("the unavailable reservations are:\n");
+							print_unslot(un_slot,queue);
+							break;
+						case 8:
+							printf("enter the patient ID:");
+							scanf("%u",&tmp1);
+							print_p(queue,tmp1);
+							break;
+						case 9:
+							printf("thank you\n");
+							break;
+						default:
+							printf("wrong input\n");
+							break;
+					}
+					if(state==9)
+						break;
+				}
+		}
+			//ADMIN mode ends here
+			
+			
+			// USER mode starets here
+		else if(tmp==2)	
+		{
+			while(1)
+			{
+				if(count1==0)
+				{
+					printf("---------------------Welcome-------------------\n");
+					count1++;
+				}
+				printf("\tto print your recordes enter 1\n");
+				printf("\tto view today`s reservations enter 2\n");
+				printf("\tto exit enter 3\n");
+				printf("\tyour choise: ");
+				scanf("%u",&state);
+				printf("-----------------------------------------------\n");
+				
+				switch(state)
+				{
+					case 1:
+						printf("please enter your ID:");
+						scanf("%u",&tmp);
+						print_p(queue,tmp);
+						break;
+					case 2:
+						printf("today`s reservations are:\n");
+						print_unslot(un_slot,queue);
+						break;
+					case 3:
+						printf("thank you\n");
+						break;
+					default:
+						printf("wrong input\n");
+						break;
+				}
+				if(state==3)
+				break;
+			}
+		}
+		else if(tmp==3)
+		{
+			printf("thank you\n");
+			break;
+		}
+		else
+			printf("\twrong input\n");
+	}
+	
+	
+		
+		
+		// USER mode ends here
+		
+		
+}
+
